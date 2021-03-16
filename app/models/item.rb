601d904a3b582e44182a00b5897cc4b7a6dd3e52
amoresca <casee.amores@gmail.com>
2021-03-16 12:57:05 -0400

    adds Tag and ItemTag models, builds associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :borrow_requests, dependent: :delete_all
+  has_many :item_tags, dependent: :delete_all
+  has_many :items, through: :item_tags
 
   validates :name, presence: true
 end
---
+++ app/models/tag.rb
@@ -0,0 +1,4 @@
+class Tag < ApplicationRecord
+  has_many :item_tags, dependent: :delete_all
+  has_many :items, through: :item_tags
+end
